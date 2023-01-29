96467f2fbbf9c1c7fae714ba73023726eba347d8
Caro <15948931+CarosCave@users.noreply.github.com>
2023-01-29 16:17:31 +0100

    Initial commit

---
+++ Tasky/TaskItem.cs
@@ -0,0 +1,6 @@
+﻿namespace Tasky;
+
+public class TaskItem
+{
+    
+}
---
+++ Tasky/UserTask.cs
@@ -0,0 +1,11 @@
+﻿namespace Tasky;
+
+public class UserTask
+{
+    public uint ID { get; private set; }
+
+    public UserTask()
+    {
+        
+    }
+}
